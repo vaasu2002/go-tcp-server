1bd91992518b6574410099c3f5d801157808b5cc
bishtvaasu <bishtvaasu@gmail.com>
2025-05-12 06:45:15 +0000

    implemented step5 handling multiple request concurrently

--- main.go
+++ main.go
@@ -16,6 +16,15 @@ import (
 // Step2 - wait for client to connection(we will use accept system call which is a blocking system call so server could not proced until some client connects)
 // Step3 - Read the request and send the response (read and write system call are blocking)
 // Step4 - Continuouly waiting for client to connect with the tcp server(will use infinite loop here)
+// We can currently process only one request at a time
+// Step5 - Handling multiple request concurrently 
+// Running in parallel
+// Once a client connects, fork a thread to process and respend, main thread should came back to accept as soon as possible
+
+// A client connects → the accept() system call returns a new socket for communication with that client.
+// Immediately after that we will creare a new Thread to (read clients req, process it, send response and close the connection(socket))
+// Meanwhile, the main thread (the one that called accept()) should not handle the client directly. Instead, it goes right back to calling accept() again to wait for the next client.
+
 
 func ops(conn net.Conn){
 	// when a client sends data to your server, the operating system places this data in a system buffer. 
@@ -49,7 +58,7 @@ func main() {
 			log.Fatalln(err)
 		}
 		fmt.Println("Connection established");
-		ops(conn)
+		go ops(conn)
 	}
-	// fmt.Println("Returning result");
 }
+ 
